eafdb9dcdbd2a3e621e69d67936c7e8cd937569b
Yami Odymel <yamiodymel@gmail.com>
2017-12-06 02:37:44 +0800

    Rate limit without the goroutine timer

--- ratelimit.go
+++ ratelimit.go
@@ -16,43 +16,34 @@ func RateLimit(conf RateLimitConfig) HandlerFunc {
 	type session struct {
 		// count 是此階段在週期內的請求數。
 		count int
-		// interval 是剩餘的週期秒數。
-		interval int
+		// start 是此階段重新計數開始的時間，用以在之後比對週期。
+		start time.Time
 	}
 	var store map[string]*session
 
-	// 透過 Goroutine 開啟新執行緒啟動一個計時器。
-	go func() {
-		// 不斷地重複。
-		for {
-			// 每一秒執行一次。
-			<-time.After(1 * time.Second)
-
-			// 遍歷整個階段流量限制存儲。
-			for _, s := range store {
-				// 如果時間到了，就重設此客戶端的計數和剩餘週期時間。
-				if s.interval <= 0 {
-					s.count = 0
-					s.interval = conf.Period
-				}
-
-				// 繼續倒數時間。
-				s.interval--
+	return func(c *Context) {
+		// reset 會重設此階段的流量限制資料。
+		reset := func() {
+			store[c.Session.ID] = &session{
+				count: 0,
+				start: time.Now(),
 			}
 		}
-	}()
 
-	return func(c *Context) {
 		// 檢查存儲裡有沒有這個階段的資料，沒有則初始化一個。
 		if _, ok := store[c.Session.ID]; !ok {
-			store[c.Session.ID] = &session{
-				count:    0,
-				interval: conf.Period,
-			}
+			reset()
+		}
+
+		// 取得階段資料裡的起始計時時間，並比對現在取得中間所消耗的時間。
+		// 如果中間消耗的時間秒數已經大於或等於設定，那麼就可以重設了。
+		duration := int(time.Since(store[c.Session.ID].start).Seconds())
+		if duration >= conf.Period {
+			reset()
 		}
 
 		// 如果請求次數大於限制就終止這個請求。
-		if store[c.Session.ID].count > conf.Limit {
+		if store[c.Session.ID].count >= conf.Limit {
 			c.AbortWithStatus(StatusTooManyRequests)
 			return
 		}
