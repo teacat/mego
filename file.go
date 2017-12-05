62a50637836c73467e791fb160d80bf5c27352cb
Yami Odymel <yamiodymel@gmail.com>
2017-12-06 03:20:40 +0800

    Added ChunkProcessor relatives

--- file.go
+++ file.go
@@ -1,5 +1,7 @@
 package mego
 
+import "os"
+
 const (
 	// B 為 Byte。
 	B = 1
@@ -23,19 +25,41 @@ type File struct {
 	Extension string
 	// Path 為此檔案上傳後的本地路徑。
 	Path string
+	// Keys 為此檔案的鍵值組，可供開發者存放自訂資料。
+	Keys map[string]interface{}
 }
 
 // Remove 會移除這個檔案。
 func (f *File) Remove() error {
-	return nil
+	return os.Remove(f.Path)
 }
 
 // Move 會移動接收到的檔案到指定路徑。
-func (f *File) Move(path string) error {
+func (f *File) Move(dest string) error {
+	return os.Rename(f.Path, dest)
+}
+
+// ChunkProcessor 是一個區塊處理介面，這令你可以自行撰寫區塊處理函式，
+// 例如將每個區塊上傳至 Amazon S3 而不是在本地拼裝。
+type ChunkProcessor interface {
+	// Process 會在接收到每個區塊的時候被呼叫，並接收區塊建構體。
+	// 當這個函式回傳了一個錯誤會中斷檔案的上傳。
+	Process(*Chunk) error
+	// Done 會在所有區塊都處理完畢時呼叫，會接收一個最後上傳的區塊建構體。
+	// 需要回傳一個有資料的檔案結構體。
+	Done(*Chunk) *File
+}
+
+// DefaultChunkProcessor 是預設的區塊處理建構體，用以實作區塊處理介面。
+type DefaultChunkProcessor struct {
+}
+
+// Process 會處理每個區塊。
+func (p *DefaultChunkProcessor) Process(chunk *Chunk) error {
 	return nil
 }
 
-// Rename 會重新命名此檔案。
-func (f *File) Rename(name string) error {
+// Done 會在最終處理區塊的收尾。
+func (p *DefaultChunkProcessor) Done(last *Chunk) *File {
 	return nil
 }
