bf666fd4dc82b61a31341794458c445ba852fbd1
Yami Odymel <yamiodymel@gmail.com>
2017-12-07 02:03:57 +0800

    Rewrote the params system

--- params.go
+++ params.go
@@ -2,100 +2,92 @@ package mego
 
 import "time"
 
-// Params 保存著接收到的參數。
-type Params struct {
-	// data 是接收到的參數切片。
-	Data []interface{}
+// Param 代表著單一個參數。
+type Param struct {
+	// data 是這個參數在型態轉換之前的介面。
+	data interface{}
+	// len 是參數陣列的長度。
+	len int
 }
 
 // Len 能夠取得參數的數量。
-func (p *Params) Len() int {
-	return len(p.Data)
+func (p *Param) Len() int {
+	return p.len
 }
 
 // Get 能夠以介面取得指定的參數。
-func (p *Params) Get(index int) (v interface{}, has bool) {
-	if !p.Has(index) {
-		v, has = nil, false
-		return
-	}
-	v, has = p.Data[index], true
-	return
+func (p *Param) Get() (v interface{}) {
+	return p.data
 }
 
 // GetBool 能夠以布林值取得指定的參數。
-func (p *Params) GetBool(index int) (v bool) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(bool)
+func (p *Param) GetBool() (v bool) {
+	if val, ok := p.data.(bool); ok {
+		v = val
 	}
 	return
 }
 
 // GetFloat64 能夠以浮點數取得指定的參數。
-func (p *Params) GetFloat64(index int) (v float64) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(float64)
+func (p *Param) GetFloat64(index int) (v float64) {
+	if val, ok := p.data.(float64); ok {
+		v = val
 	}
 	return
 }
 
 // GetInt 能夠以正整數取得指定的參數。
-func (p *Params) GetInt(index int) (v int) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(int)
+func (p *Param) GetInt(index int) (v int) {
+	if val, ok := p.data.(int); ok {
+		v = val
 	}
 	return
 }
 
 // GetString 能夠以字串取得指定的參數。
-func (p *Params) GetString(index int) (v string) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(string)
+func (p *Param) GetString(index int) (v string) {
+	if val, ok := p.data.(string); ok {
+		v = val
 	}
 	return
 }
 
 // GetStringMap 能夠以 `map[string]interface{}` 取得指定的參數。
-func (p *Params) GetStringMap(index int) (v map[string]interface{}) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(map[string]interface{})
+func (p *Param) GetStringMap(index int) (v map[string]interface{}) {
+	if val, ok := p.data.(map[string]interface{}); ok {
+		v = val
 	}
 	return
 }
 
 // GetMapString 能夠以 `map[string]string` 取得指定的參數。
-func (p *Params) GetMapString(index int) (v map[string]string) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(map[string]string)
+func (p *Param) GetMapString(index int) (v map[string]string) {
+	if val, ok := p.data.(map[string]string); ok {
+		v = val
 	}
 	return
 }
 
 // GetStringSlice 能夠以字串切片取得指定的參數。
-func (p *Params) GetStringSlice(index int) (v []string) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.([]string)
+func (p *Param) GetStringSlice(index int) (v []string) {
+	if val, ok := p.data.([]string); ok {
+		v = val
 	}
 	return
 }
 
 // GetTime 能夠以時間取得指定的參數。
-func (p *Params) GetTime(index int) (v time.Time) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(time.Time)
+func (p *Param) GetTime(index int) (v time.Time) {
+	if val, ok := p.data.(time.Time); ok {
+		v = val
 	}
 	return
 }
 
 // GetDuration 能夠以時間長度取得指定的參數。
-func (p *Params) GetDuration(index int) (v time.Duration) {
-	if val, ok := p.Get(index); ok && val != nil {
-		v, _ = val.(time.Duration)
+func (p *Param) GetDuration(index int) (v time.Duration) {
+	if val, ok := p.data.(time.Duration); ok {
+		v = val
 	}
 	return
-}
-
-// Has 會返回一個指定的參數是否存在的布林值。
-func (p *Params) Has(index int) bool {
-	return len(p.Data) >= index
 }
