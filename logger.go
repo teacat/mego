cb9451355b271e45e308cbbf56d4284f6ef1307a
Yami Odymel <yamiodymel@gmail.com>
2017-12-26 08:20:52 +0800

    WIP recovery, logger init func

--- logger.go
+++ logger.go
@@ -1,1 +1,7 @@
 package mego
+
+// Logger 會回傳一個紀錄中介軟體，能夠簡略地紀錄每個請求的 IP 和日期與耗費時間。
+// 亦能傳入自訂的 `*io.Writer` 決定紀錄的輸出位置（如：`Stdout` 或檔案）。
+func Logger() {
+
+}
--- recovery.go
+++ recovery.go
@@ -1,1 +1,7 @@
 package mego
+
+// Recovery 會回傳一個回覆中介軟體，
+// 這能在請求發生 `panic` 錯誤時自動回復並顯示相關錯誤訊息供除錯，而不會導致整個伺服器強制關閉。
+func Recovery() {
+
+}
