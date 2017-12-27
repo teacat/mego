86f8e93eddbf812ae0b2cac4709df4c42c26697f
Yami Odymel <yamiodymel@gmail.com>
2017-12-27 21:24:48 +0800

    Removed the Chunker function, ready to built-in

--- file.go
+++ file.go
@@ -38,35 +38,3 @@ func (f *File) Remove() error {
 func (f *File) Move(dest string) error {
 	return os.Rename(f.Path, dest)
 }
-
-// ChunkProcessor 是一個區塊處理介面，這令你可以自行撰寫區塊處理函式，
-// 例如將每個區塊上傳至 Amazon S3 而不是在本地拼裝。
-type ChunkProcessor interface {
-	// Process 會在接收到每個區塊的時候被呼叫，並接收區塊建構體。
-	// 當這個函式回傳了一個錯誤會中斷檔案的上傳。
-	Process(*Chunk) error
-	// Done 會在所有區塊都處理完畢時呼叫，會接收一個最後上傳的區塊建構體。
-	// 需要回傳一個有資料的檔案結構體。
-	Done(*Chunk) *File
-}
-
-// DefaultChunkProcessor 是預設的區塊處理建構體，用以實作區塊處理介面。
-type DefaultChunkProcessor struct {
-}
-
-// Process 會處理每個區塊。
-func (p *DefaultChunkProcessor) Process(chunk *Chunk) error {
-	return nil
-}
-
-// Done 會在最終處理區塊的收尾。
-func (p *DefaultChunkProcessor) Done(last *Chunk) *File {
-	return nil
-}
-
-// Chunker 會在方法中處理接收到的區塊資料並組成檔案。
-func Chunker() HandlerFunc {
-	return func(c *Context) {
-		c.Next()
-	}
-}
