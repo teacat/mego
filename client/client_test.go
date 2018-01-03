6588d2a4ce2f140974cb690ecd6897675a34655c
Yami Odymel <yamiodymel@gmail.com>
2018-01-04 03:34:02 +0800

    Added testing server in Go client test

--- client/client_test.go
+++ client/client_test.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/TeaMeow/Mego"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,8 +16,80 @@ var client *Client
 type dataStruct struct {
 	Username, Password string
 }
-type respStruct struct {
-	Username, Password string
+
+func init() {
+	go func() {
+		e := mego.New()
+		e.Register("Test", func(c *mego.Context) {
+			var d dataStruct
+			c.MustBind(&d)
+			c.Respond(d)
+		})
+		e.Register("TestParams", func(c *mego.Context) {
+			c.Respond(dataStruct{
+				Username: c.Param(0).GetString(),
+				Password: c.Param(1).GetString(),
+			})
+		})
+		e.Register("TestFile", func(c *mego.Context) {
+			p := c.MustGetFile("File1").Path
+			f, err := os.Open(p)
+			if err != nil {
+				panic(err)
+			}
+			b, err := ioutil.ReadAll(f)
+			if err != nil {
+				panic(err)
+			}
+			c.Respond(b)
+		})
+		e.Register("TestFiles", func(c *mego.Context) {
+			f := c.MustGetFiles("File1")
+			f2 := c.MustGetFiles("File2")
+			c.Respond(len(f) + len(f2))
+		})
+		e.Register("TestFileSlice", func(c *mego.Context) {
+			f := c.MustGetFiles("File1")
+			c.Respond(len(f))
+		})
+		e.Register("TestFileMix", func(c *mego.Context) {
+			var d dataStruct
+			c.MustBind(&d)
+
+			f := c.MustGetFiles("File1")
+			f2 := c.MustGetFiles("File2")
+			c.Respond(H{
+				"Files":    len(f) + len(f2),
+				"Username": d.Username,
+				"Password": d.Password,
+			})
+		})
+		e.Register("TestFileChunks", func(c *mego.Context) {
+			p := c.MustGetFile("File1").Path
+			f, err := os.Open(p)
+			if err != nil {
+				panic(err)
+			}
+			b, err := ioutil.ReadAll(f)
+			if err != nil {
+				panic(err)
+			}
+			c.Respond(b)
+		})
+		e.Register("TestFileChunksMix", func(c *mego.Context) {
+			var d dataStruct
+			c.MustBind(&d)
+
+			f := c.MustGetFiles("File1")
+			c.Respond(H{
+				"Files":    len(f),
+				"Username": d.Username,
+				"Password": d.Password,
+			})
+		})
+	}()
+	// 等待一秒讓引擎初始化並運作。
+	<-time.After(time.Second * 1)
 }
 
 func TestClientMain(t *testing.T) {
@@ -41,7 +116,7 @@ func TestClientReconnect(t *testing.T) {
 }
 
 func TestClientSendStruct(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.
 		Call("Test").
@@ -51,28 +126,28 @@ func TestClientSendStruct(t *testing.T) {
 		}).
 		EndStruct(&resp)
 	assert.NoError(err)
-	assert.Equal(respStruct{
+	assert.Equal(dataStruct{
 		Username: "YamiOdymel",
 		Password: "yamiodymel12345",
 	}, resp)
 }
 
 func TestClientSendJSON(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.
 		Call("Test").
 		Send(`{"Username": "YamiOdymel", "Password": "yamiodymel12345"}`).
 		EndStruct(&resp)
 	assert.NoError(err)
-	assert.Equal(respStruct{
+	assert.Equal(dataStruct{
 		Username: "YamiOdymel",
 		Password: "yamiodymel12345",
 	}, resp)
 }
 
 func TestClientSendMap(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.
 		Call("Test").
@@ -82,35 +157,35 @@ func TestClientSendMap(t *testing.T) {
 		}).
 		EndStruct(&resp)
 	assert.NoError(err)
-	assert.Equal(respStruct{
+	assert.Equal(dataStruct{
 		Username: "YamiOdymel",
 		Password: "yamiodymel12345",
 	}, resp)
 }
 
 func TestClientSendParamsSlice(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.
 		Call("TestParams").
 		Send([]string{"YamiOdymel", "yamiodymel12345"}).
 		EndStruct(&resp)
 	assert.NoError(err)
-	assert.Equal(respStruct{
+	assert.Equal(dataStruct{
 		Username: "YamiOdymel",
 		Password: "yamiodymel12345",
 	}, resp)
 }
 
 func TestClientSendParamsJSON(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.
 		Call("TestParams").
 		Send(`["YamiOdymel", "yamiodymel12345"]`).
 		EndStruct(&resp)
 	assert.NoError(err)
-	assert.Equal(respStruct{
+	assert.Equal(dataStruct{
 		Username: "YamiOdymel",
 		Password: "yamiodymel12345",
 	}, resp)
@@ -303,40 +378,34 @@ func TestClientSendFileChunksMix(t *testing.T) {
 }
 
 func TestClientSubscribe(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
-	err := client.Subscribe("TestEvent", "TestChannel", dataStruct{
-		Username: "YamiOdymel",
-		Password: "yamiodymel12345",
-	})
+	err := client.Subscribe("TestEvent", "TestChannel")
 	assert.NoError(err)
 }
 
 func TestClientSubscribeError(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
-	err := client.Subscribe("TestRefuseEvent", "TestChannel", dataStruct{
-		Username: "YamiOdymel",
-		Password: "yamiodymel12345",
-	})
+	err := client.Subscribe("TestRefuseEvent", "TestChannel")
 	assert.Error(err)
 	assert.Equal(ErrSubscriptionRefused, err)
 }
 
 func TestClientOn(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	client.On("TestEvent", func(e *Event) {})
 }
 
 func TestClientOff(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	client.Off("TestEvent")
 }
 
 func TestClientUnsubscribe(t *testing.T) {
-	var resp respStruct
+	var resp dataStruct
 	assert := assert.New(t)
 	err := client.Unsubscribe("TestEvent", "TestChannel")
 	assert.NoError(err)
